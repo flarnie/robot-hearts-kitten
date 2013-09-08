571f3f0d5951b4f831e7563f4e44da58ffdf94dd
Flarnie Marchan <fnonemaker@gmail.com>
2013-09-07 19:38:05 -0700

    Setup Backbone and empty home canvas, tweak styles, fix footer style

---
+++ app/assets/javascripts/rhk.js
@@ -0,0 +1,15 @@
+window.Rhk = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    	console.log('Hello from Backbone!');
+
+			
+  }
+};
+
+$(document).ready(function(){
+  Rhk.initialize();
+});
