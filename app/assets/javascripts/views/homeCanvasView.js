56f7d4ad63fa8528c95c03e8631516c2ac76313c
Flarnie Marchan <fnonemaker@gmail.com>
2013-09-08 07:27:00 -0700

    Finish robot kitten animation on homepage

--- app/assets/javascripts/views/homeCanvasView.js
+++ app/assets/javascripts/views/homeCanvasView.js
@@ -7,38 +7,42 @@ Rhk.Views.HomeCanvasView = Backbone.View.extend({
 		this.ctx = theCanvas.getContext("2d");
 		//the actors
 		var that = this;
+		// robot
+		var robotImg = new Image();
+		robotImg.src = "robot.gif";
+		var robot = new Rhk.Models.Movable({
+			ctx: that.ctx,
+			imageObj: robotImg,
+			position: [0, (that.canvas_height / 2)],
+			height: that.imgWidth * 2,
+			width: that.imgWidth * 2
+		});
+		//
+		// kitten 
 		var kittenImg = new Image();
 		kittenImg.src = "kitten.gif";
 		var kitten = new Rhk.Models.Movable({
 			ctx: that.ctx,
 			imageObj: kittenImg,
-			position: [100, 100],
+			position: [(that.canvas_width / 2), (that.canvas_height / 2)],
 			height: that.imgWidth,
 			width: that.imgWidth
 		});
 		//the collection of actors
-		this.actors = new Rhk.Collections.Movables([kitten]);
+		this.actors = new Rhk.Collections.Movables([robot, kitten]);
 	},
 	step: function () {
 		var that = this;
 		this.actors.each(function (actor) {
 			actor.move("east", 3);
-			//NOTE: 700 is canvas width
-			//Catch them and make them wrap
+			//NOTE: Catch them and make them wrap
 			if (actor.position[0] > that.canvas_width){
-				actor.position[0] = 0;
+				actor.position[0] = -1 * (that.canvas_width);
 			}
 		});
 	},
 	render: function () {
 		var that = this;
-		//TODO: make robot an actor object in the collection
-		var robotImg = new Image();
-		robotImg.src = "robot.gif";
-		robotImg.onload = function () {
-			that.ctx.drawImage(robotImg, 0, 0, that.imgWidth + 90, that.imgWidth + 90);
-		}
-		//
 		this.ctx.clearRect(0, 0, this.canvas_width, this.canvas_height);
 		this.actors.each(function (actor) {
 			actor.drawSelf();
@@ -57,7 +61,7 @@ Rhk.Views.HomeCanvasView = Backbone.View.extend({
 			// that.actors.each(function (actor) {
 			// 	console.log(actor.position);
 			// });
-	  }, 62);
+	  }, 22);
 	}
 	
 });
