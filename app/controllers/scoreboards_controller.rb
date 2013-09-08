277992619731b28b8b634e6bb58b434a62f3c951
Flarnie Marchan <fnonemaker@gmail.com>
2013-09-07 18:10:28 -0700

    Finish styles and views for scoreboards index and show

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,11 +1,11 @@
 class ScoreboardsController < ApplicationController
 
   def index
-    
+    @scoreboards = ScoreBoard.all
   end
   
   def show
-    
+    @scoreboard = ScoreBoard.find(params[:id])
   end
 
 end
