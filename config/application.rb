94fb8db600b40bf7b2b79b88632088b4ccd8e778
Flarnie Marchan <fnonemaker@gmail.com>
2013-09-12 09:52:27 -0700

    Attempt to fix heroku precompile issue

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Rhk
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    #tweak to make assets precompile on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
