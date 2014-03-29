8b5b0d8bdeda27d6b2329a8756430cd4b683ae46
Flarnie Marchan <fnonemaker@gmail.com>
2014-03-29 07:28:31 -0700

    Remove autogenerated Rails testing files.

--- test/unit/helpers/scoreboards_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ScoreboardsHelperTest < ActionView::TestCase
-end
--- test/unit/score_board_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ScoreBoardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
