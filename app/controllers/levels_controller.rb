b29c5619fdcd525521b6f1dbd46a9e32b64bd6a3
Flarnie Marchan <fnonemaker@gmail.com>
2014-04-12 20:51:05 -0700

    Fix typo and remove puts statement.

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -3,17 +3,16 @@ class LevelsController < ApplicationController
   def index
     @levels = Level.order("level_order ASC")
   end
-  
+
   def show
     @level = Level.find(params[:id])
-    #TODO: place_random_kitten before rander, clear it after render.
+    #TODO: place_random_kitten before render, clear it after render.
     respond_to do |format|
       format.json { render "levels/show.rabl" }
       #TODO: get rable template to work!
       # format.json { render :json => @level }
       format.html { render :show }
     end
-    puts "after the render"
   end
 
 end
