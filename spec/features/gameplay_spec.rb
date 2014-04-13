bef2afcb3664997a00f5ac425aaeb80eca218173
Flarnie Marchan <fnonemaker@gmail.com>
2014-04-12 20:52:26 -0700

    Fix gameplay spec for level 1 to use new `keypress` method.

--- spec/features/gameplay_spec.rb
+++ spec/features/gameplay_spec.rb
@@ -6,13 +6,13 @@ describe "Playing level 1" do
     expect(page).to have_content "COOKIES: 0"
   end
 
-  it "can be won by having robot go 35 steps up and 30 steps right", :js => true do
+  it "can be won by having robot go 35 steps up and 32 steps right", :js => true do
     visit level_path(1)
     35.times do
-      simulate_keypress("38")
+      press_key(:Up)
     end
-    30.times do
-      simulate_keypress("39")
+    32.times do
+      press_key(:Right)
     end
     expect(page).to have_content "You found the kitten!"
   end
